7e93b95f3f6da10cafb2101fdc3fb20a57e4ce4f
libo929 <libo929@163.com>
2020-06-05 15:59:11 +0800

    Change the name of binary.

--- batchRun.sh
+++ batchRun.sh
@@ -8,6 +8,6 @@ while [ ${Ep} -le 2400 ]; do
 #while [ ${Ep} -le 2000 ]; do
     echo ${Ep}
 	sed -e "s;\ PROTONENERGY;\ ${Ep};" -e "s;\ FILENAME;\ run${Ep};" template.mac > run${Ep}.mac
-	./Activation run${Ep}.mac
+	./NeutronGen run${Ep}.mac
 	let Ep+=20
 done
