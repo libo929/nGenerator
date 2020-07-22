d72f218fff3c0405c97b6fc4f27e574b1a03822a
Bo Li <libo0929@gmail.com>
2020-07-22 15:10:05 +0800

    Run task in multi-thread mode

--- batchRun.sh
+++ batchRun.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
-echo "Hello"
+exeName=NeutronGen
 
-Ep=1900
+nap()
+{
+        exeProc=`ps -U $USER | grep ${exeName} | wc -l`
 
-while [ ${Ep} -le 2400 ]; do
-#while [ ${Ep} -le 2000 ]; do
-    echo ${Ep}
-	sed -e "s;\ PROTONENERGY;\ ${Ep};" -e "s;\ FILENAME;\ run${Ep};" template.mac > run${Ep}.mac
-	./NeutronGen run${Ep}.mac
-	let Ep+=20
+        while [ $exeProc -ge 6 ]; do
+		sleep 5
+		exeProc=`ps -U $USER | grep ${exeName} | wc -l`
+        done
+}
+
+Ei=300
+
+while [ ${Ei} -le 600 ]; do
+	index=0
+        while [ ${index} -le 9 ]; do
+		let seed=${Ei}+${index}
+	        echo "Ei: "${Ei} ", seed:"${seed}
+		let index+=1
+	        sed -e "s;\ PARTICLEENERGY;\ ${Ei};" -e "s;\ FILENAME;\ run${Ei}_${seed};" template.mac > run${Ei}_${seed}.mac
+	        ./${exeName} run${Ei}_${seed}.mac ${seed} > run${Ei}_${seed}.log 2>&1 &
+	        nap
+	done
+
+	let Ei+=20
 done
+
+
+
